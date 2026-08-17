9417ce5c30648f2cd7c591d9a18941d89a43d545
Christian <kalonjic34@gmail.com>
2026-08-17 15:23:27 +0200

    Add routes for contact management functionality

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/',function(){
+    $html ="
+    <h1>Contact Management App</h1>
+    <div>
+        <a href='" .route('contacts.index')."'>All contacts</a>
+        <a href='".route('contacts.create')."'>Add contacts</a>
+        <a href='".route('contacts.show',1)."'>Show contacts</a>
+    </div>
+    ";
+    return  $html;
+});
+
+Route::get('/contact/',function(){
+    return '<h1>All contacts</h1>';
+})->name('contacts.index');
+
+
+Route::get('/contacts/create',function(){
+    return '<h1>Add new contacts</h1>';
+})->name('contacts.create');
+
+Route::get('/contacts/{id}',function($id){
+    return 'Contact: '.$id;
+})->name('contacts.show');
+
+
