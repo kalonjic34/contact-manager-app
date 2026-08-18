e362aea4632470fe0011de6506b39efae715496c
Christian <kalonjic34@gmail.com>
2026-08-18 17:44:06 +0200

    Add company selection and search functionality to contacts filter view

---
+++ resources/views/contacts/_company-select.blade.php
@@ -0,0 +1,7 @@
+<select class="custom-select">
+    <option value="" selected>All Companies</option>
+        <?php foreach ($companies as $id => $company):?>
+            <option value="{{ $id }}">{{ $company['name'] }}</option>
+        <?php endforeach ?>
+
+</select>
