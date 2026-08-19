158efd9bea3b92122209b9d059cdaebeea92210f
Christian <kalonjic34@gmail.com>
2026-08-19 16:26:55 +0200

    Refactor routes to use ContactController for contact management

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,21 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-function get_contacts(){
-    return [
-        1=>['id'=> 1,'name'=>"Name 1", 'phone'=>'0616750088'],
-        2=>['id'=> 2,'name'=>"Name 2", 'phone'=>'0769753488'],
-        3=>['id'=> 3,'name'=>"Name 3", 'phone'=>'0716521431'],
-    ];
-}
+
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact/',function(){
-
-    $companies = [
-        1=>['name'=>"Company One", 'contacts'=>'3'],
-        2=>['name'=>"Company Two", 'contacts'=>'5'],
-
-    ];
-    $contacts = get_contacts();
-
-    return view('contacts.index', compact('contacts','companies'));
-})->name('contacts.index');
+Route::get('/contact/',[ContactController::class,'index'])->name('contacts.index');
 
 
-Route::get('/contacts/create',function(){
-    return view('contacts.create');
-})->name('contacts.create');
+Route::get('/contacts/create',[ContactController::class,'create'])->name('contacts.create');
 
-Route::get('/contacts/{id}',function($id){
-
-    $contacts = get_contacts();
-
-    abort_unless(isset($contacts[$id]),404);
-
-    $contact = $contacts[$id];
-
-    return view('contacts.show')->with('contact',$contact);
-})->name('contacts.show');
+Route::get('/contacts/{id}', [ContactController::class,'show'])->name('contacts.show');
 
 Route::fallback(function(){
     return '<h1>Sorry, the page does not exist</h1>';
