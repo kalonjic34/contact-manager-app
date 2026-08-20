83689eb95cd8f00d3f831bdaa5809684eec4705f
Christian <kalonjic34@gmail.com>
2026-08-20 13:57:59 +0200

    Refactor routes in web.php to organize resource routes for companies, tags, tasks, and activities

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactNoteController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +20,27 @@ Route::get('/contacts/create',[ContactController::class,'create'])->name('contac
 
 Route::get('/contacts/{id}', [ContactController::class,'show'])->name('contacts.show');
 
+Route::resource('/companies',CompanyController::class);
 
+Route::resources([
+    '/tags'=> TagController::class,
+    '/task'=> TaskController::class,
+]);
+
+// Route::resource('/activities', ActivityController::class)->except([
+//     'index','show'
+// ]);
+
+// Route::resource('/activities', ActivityController::class)->names([
+//     'index'=>'activities.all',
+//     'show'=>'activities.view',
+// ]);
+
+Route::resource('/activities', ActivityController::class)->parameters([
+    'activities'=>'active'
+]);
+
+Route::resource('contacts.notes',ContactNoteController::class)->shallow();
 
 Route::fallback(function(){
     return '<h1>Sorry, the page does not exist</h1>';
