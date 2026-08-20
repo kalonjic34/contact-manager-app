6432226c1496621c959183f570efe56bf8646570
Christian <kalonjic34@gmail.com>
2026-08-20 13:09:00 +0200

    Refactor root route to use WelcomeController for rendering the welcome view

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', WelcomeController::class);
+    
 Route::get('/contact/',[ContactController::class,'index'])->name('contacts.index');
-
 
 Route::get('/contacts/create',[ContactController::class,'create'])->name('contacts.create');
 
 Route::get('/contacts/{id}', [ContactController::class,'show'])->name('contacts.show');
+
+
 
 Route::fallback(function(){
     return '<h1>Sorry, the page does not exist</h1>';
